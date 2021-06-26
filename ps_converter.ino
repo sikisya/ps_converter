5e96ff543f9844ab3cc8240339663d25f5f815b2
sikisya <sikisya@users.noreply.github.com>
2021-06-26 14:49:49 +0900

    feat: BitBang版に変更
    - XIAO対応
    - TinyUSB対応予定

--- ps_converter.ino
+++ ps_converter.ino
@@ -1,11 +1,22 @@
-#include <PsxControllerBitBang.h> // v0.3.0
+//#include <PsxControllerHwSpi.h> // v0.3.0
+#include <PsxControllerBitBang.h>
+#include <Adafruit_TinyUSB.h>
 
-const byte PIN_PS2_DAT = 5;
-const byte PIN_PS2_CMD = 4;
-const byte PIN_PS2_ATT = 3;
-const byte PIN_PS2_CLK = 2;
+const byte PIN_PS2_DAT = 10;
+const byte PIN_PS2_CMD = 9;
+const byte PIN_PS2_CLK = 8;
+const byte PIN_PS2_ATT = 7;
 
 PsxControllerBitBang<PIN_PS2_ATT, PIN_PS2_CMD, PIN_PS2_DAT, PIN_PS2_CLK> psx;
+//PsxControllerHwSpi<PIN_PS2_ATT> psx;
+
+uint8_t const desc_hid_report[] =
+{
+  TUD_HID_REPORT_DESC_GAMEPAD()
+};
+
+Adafruit_USBD_HID usb_hid;
+hid_gamepad_report_t gp;
 
 bool haveController = false;
 
@@ -22,6 +33,11 @@ void printInput(PsxController *psx)
     //case PSPROTO_FLIGHTSTICK:
     case PSPROTO_NEGCON:
       str = "NEGCON: ";
+
+      if (psx->buttonPressed(PSB_PAD_LEFT))
+      {
+        gp.hat = hid_gamepad_hat_t.GAMEPAD_HAT_LEFT;
+      }
       str += (psx->buttonPressed(PSB_PAD_LEFT))  ? "←" : "　";
       str += (psx->buttonPressed(PSB_PAD_DOWN))  ? "↓" : "　";
       str += (psx->buttonPressed(PSB_PAD_UP))    ? "↑" : "　";
@@ -54,19 +70,43 @@ void printInput(PsxController *psx)
       break;
   }
   Serial.println(str);
+  usb_hid.sendReport(0, &gp, sizeof(gp));
+}
+
+void resetGamePad()
+{
+  gp.x = 0;
+  gp.y = 0;
+  gp.z = 0;
+  gp.rx = 0;
+  gp.ry = 0;
+  gp.rz = 0;
+  gp.hat = 0;
+  gp.button = 0;
 }
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("setup");
+
+  usb_hid.setPollInterval(2);
+  usb_hid.setReportDescriptor(desc_hid_report, sizeof(desc_hid_report));
+  usb_hid.begin();
+
+  while( !USBDevice.mounted() )
+  {
+    delay(1);
+  }
 }
 
 void loop()
 {
+  resetGamePad();
+  
   if (!haveController)
   {
-    if (psx.begin ())
+    if (psx.begin())
     {
       Serial.println("Controller found!");
       if (!psx.enterConfigMode ())
